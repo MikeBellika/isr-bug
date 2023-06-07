9aa40d292d881105c8498860ece7decf38e6721c
Mike Bellika <mike@altinget.dk>
2023-06-07 09:47:58 +0200

    Add ISR on /hello

--- nitro.config.ts
+++ nitro.config.ts
@@ -4,6 +4,7 @@ import { defineNitroConfig } from 'nitropack/config'
 export default defineNitroConfig({
   // Nitro options
   routeRules: {
-    '/': { isr: 60 }
+    '/': { isr: 60 },
+    '/hello': { isr: 60 }
   }
 })
